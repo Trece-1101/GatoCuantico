97119a27cbd964494eeeaa7fe6b89d716973ae45
Trece-1101 <omar.bazzi.sf@gmail.com>
2021-09-03 22:01:23 -0300

    main menu

--- game/menus/MainMenu.gd
+++ game/menus/MainMenu.gd
@@ -4,6 +4,15 @@ export(String, FILE, "*.tscn") var level_selection_menu = ""
 export(String, FILE, "*.tscn") var credits_menu = ""
 export(String, FILE, "*.tscn") var controls_menu = ""
 
+var counter := 0
+
+var texts := [
+	"En 1935 Erwin Schrodinger ideo un experimento mental descrito como una paradoja. Dicho experimento fue denominado 'El gato de schrodinger'",
+	"El escenario presenta un gato hipotetico que puede estar simultaneamente vivo y muerto. A este estado se lo conoce como superposicion cuantica",
+	"En la mecanica cuantica la teoria predominante, llamada interpretacion de Copenhague, establece que un sistema cuantico permanece en superposicion hasta que es observado",
+	"Cuando es observado, la superposicion colapsa en uno u otro de los posibles estados definidos. La sola accion de observar modifica el estado de un sistema.",
+	"Si no podemos observar. ¿Podemos controlar el resultado? ¿Y si el solo hecho de no observarlo es una forma de control? ¿O acaso el control esta limitado a observarlo y colapsar en un estado?"
+]
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
@@ -30,3 +39,26 @@ func _on_Controls_pressed() -> void:
 
 func _on_PlayTut_pressed() -> void:
 	get_tree().change_scene("res://game/levels/Tutorial/Level1Tutorial.tscn")
+
+
+func _on_BottomLeft_mouse_entered() -> void:
+	$BottomLeft.color = Color.white
+	give_text($BottomLeft/Label)
+
+
+func _on_BottomRight_mouse_entered() -> void:
+	$BottomRight.color = Color.white
+	give_text($BottomRight/Label)
+
+func give_text(label:Label) -> void:
+	if counter >= texts.size():
+		counter = 0
+	label.text = texts[counter]
+	counter += 1
+
+func _on_BottomLeft_mouse_exited() -> void:
+	$BottomLeft.color = Color.black
+
+
+func _on_BottomRight_mouse_exited() -> void:
+	$BottomRight.color = Color.black
